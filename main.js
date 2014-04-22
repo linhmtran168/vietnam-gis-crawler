bce265be172c25fa59c40c90204f2f5b14224408
Linh Tran <linh.mtran168@live.com>
2014-04-22 16:48:40 +0700

    refactor the data format

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var _ = require('lodash'),
+var util = require('utils'),
+  fs = require('fs'),
   casper = require('casper').create({
     verbose: true,
     logLevel: 'debug',
@@ -12,21 +13,29 @@ var getData = function() {
       nodes = $('#dtree1>.dTreeNode');
 
    dataList = _.map(nodes, function(n) {
-    var result = {}, childs,
-        text = $(n).text(),
-        childNodeId = $(n).children()[3].id.slice(5);
+    var result, children,
+      name = $(n).text(),
+      childNodeId = $(n).children()[3].id.slice(5);
 
-    result.province = text;
-    result.districts = [];
-    result.id = childNodeId - 1;
+    result = {
+      id: childNodeId - 1,
+      name: name,
+      districts: []
+    };
 
-    childs = $('#dtree' + childNodeId).children();
+    // Get children nodes (districts)
+    children = $('#dtree' + childNodeId).children();
 
-    console.log('Province: ' + text);
+    console.log('Province: ' + name);
 
-    result.districts = _.map(childs, function(c) {
-      console.log('------------- ' + $(c).text());
-      return $(c).text();
+    // Add the children nodes
+    result.districts = _.map(children, function(c) {
+      var id = $(c).children()[3].id.slice(5),
+        name = $(c).text();
+
+      console.log('------------- ' + name);
+
+      return { id: id, name: name };
     });
 
     return result;
@@ -35,11 +44,18 @@ var getData = function() {
   return dataList;
 };
 
+var removeTTTP = function() {
+  var i;
+  for(i = 0; i < 5; i++) {
+    data[i].districts.splice(0, 1);
+  }
+};
+
 
 casper.start('http://gis.chinhphu.vn/');
 
 casper.on('remote.message', function(msg) {
-    this.echo('remote message caught: ' + msg);
+  this.echo('remote message caught: ' + msg);
 });
 
 // Must wait for all the tree nodes to load
@@ -50,18 +66,28 @@ casper.waitForSelector('.dTreeNode', function() {
 
 casper.then(function() {
   var i;
+  // After each click the tree will create a new node so there is total 126 nodes.
+  // The original nodes are the ones with odd number
   for (i = 1; i < 126; i += 2) {
     this.click('#dtree1 .dTreeNode:nth-child(' + i + ') a:nth-child(2)');
   }
 });
 
+// Wait for the last node to load then load the data from those nodes
 casper.waitForSelector('#dtree64', function() {
   data = this.evaluate(getData);
+  // Remove additional districts 'TTTP' for 5 cities
+  removeTTTP();
+});
+
+// Write to file
+casper.then(function() {
+  // Dump the data to json
+  var dataStr = JSON.stringify(data);
+  fs.write('data.json', dataStr);
 });
 
 casper.run(function() {
-  var that = this;
-  this.echo(data.length + ' links found:');
-  this.echo(' - ' + data.join('\n - '));
+  this.echo(data.length + ' provinces found:');
   this.exit();
 });
